56964324c3df0ae02984eea69739ac7f9c4d3631
dwclake <clake.dw@gmail.com>
2023-10-09 15:45:49 -0700

    update: indentation

--- src/interpreter/scanner.zig
+++ src/interpreter/scanner.zig
@@ -6,24 +6,24 @@ const std = @import("std");
 const Token = @import("token.zig").Token;
 
 pub const Scanner = struct {
-  const Self = @This();
+    const Self = @This();
 
-  source: []u8,
-  tokens: std.ArrayList(Token),
+    source: []u8,
+    tokens: std.ArrayList(Token),
 
-  pub fn init(allocator: *const std.mem.Allocator, source: []u8) Self {
-    return Self{
-      .source = source,
-      .tokens = std.ArrayList(Token).init(allocator.*),
-    };
-  }
+    pub fn init(allocator: *const std.mem.Allocator, source: []u8) Self {
+        return Self{
+            .source = source,
+            .tokens = std.ArrayList(Token).init(allocator.*),
+        };
+    }
 
-  pub fn deinit(self: *Self) void {
-    self.tokens.deinit(); 
-  }
+    pub fn deinit(self: *Self) void {
+        self.tokens.deinit(); 
+    }
 
-  pub fn scan(self: *Self) !std.ArrayList(Token) {
-    try self.tokens.append(Token{.identifier = self.source});
-    return self.tokens;
-  }
+    pub fn scan(self: *Self) !std.ArrayList(Token) {
+        try self.tokens.append(Token{.identifier = self.source});
+        return self.tokens;
+    }
 };
