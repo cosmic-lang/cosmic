8ca2772e48a6dda65bdbc8f37e256575848c8e9a
dwclake <clake.dw@gmail.com>
2023-10-21 09:09:46 -0700

    update: file handling

--- src/utilities/file.rs
+++ src/utilities/file.rs
@@ -1,8 +1,8 @@
-use std::{path::PathBuf, env};
+use std::{path::PathBuf, env, rc::Rc};
 
 use anyhow::{Result, anyhow};
 
-pub fn read_file(path: &PathBuf, ext: &str) -> Result<String> {
+pub fn read_file(path: &PathBuf, ext: &str) -> Result<Rc<str>> {
     let path = env::current_dir()?.join(path);
     let extension = path.extension().unwrap().to_str().unwrap();
 
@@ -10,10 +10,10 @@ pub fn read_file(path: &PathBuf, ext: &str) -> Result<String> {
       return Err(anyhow!(format!("Invalid extension: {extension}, expected: {ext}")))
     }
 
-    let source: String;
+    let source: Rc<str>;
     match path.to_str() {
       Some(path) => match std::fs::read_to_string(path) {
-        Ok(src) => source = src,
+        Ok(src) => source = src.into(),
         Err(msg) => return Err(anyhow!(msg))
       },
       None => return Err(anyhow!("Could not convert path to string")) 
